0c04e9ec006f8b0b9e4bd7a5de03e417053517c8
DeltaCubed <diamondspectreme@gmail.com>
2020-07-08 12:56:40 -0700

    added more boards

--- SudokuBoards.java
+++ SudokuBoards.java
@@ -22,9 +22,13 @@ public class SudokuBoards {
             { 0, 0, 0, 3, 0, 0, 0, 7, 0 }, { 0, 8, 0, 1, 0, 0, 9, 0, 0 }, { 6, 0, 0, 0, 0, 9, 0, 4, 8 },
             { 9, 0, 4, 6, 0, 2, 0, 1, 0 }, { 0, 1, 0, 0, 6, 3, 0, 0, 0 }, { 3, 0, 0, 9, 0, 5, 8, 2, 1 },
             { 4, 2, 0, 0, 0, 0, 3, 0, 0 } };
+    private int[][] board6 = { { 3, 8, 5, 0, 0, 0, 0, 0, 0 }, { 9, 2, 1, 0, 0, 0, 0, 0, 0 },
+            { 6, 4, 7, 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 1, 2, 3, 0, 0, 0 }, { 0, 0, 0, 7, 8, 4, 0, 0, 0 },
+            { 0, 0, 0, 6, 9, 5, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0, 8, 7, 3 }, { 0, 0, 0, 0, 0, 0, 9, 6, 2 },
+            { 0, 0, 0, 0, 0, 0, 1, 4, 5 } };
 
     public int[][] pickBoard() {
-        switch (/* (int) (Math.random() * 4) */ 5) {
+        switch ((int) (Math.random() * 4)) {
             case 1:
                 return board1;
             case 2:
@@ -33,8 +37,10 @@ public class SudokuBoards {
                 return board3;
             case 4:
                 return board4;
-            default:
+            case 5:
                 return board5;
+            default:
+                return board6;
         }
     }
 }
